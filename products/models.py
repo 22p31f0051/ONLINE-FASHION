ec0357430e232f7ead79e34bed276149001bb6f9
22p31f0051 <pvsivadurgapilli@gmail.com>
2024-02-24 12:01:02 +0530

    final

--- products/models.py
+++ products/models.py
@@ -6,7 +6,6 @@ class ProductList(models.Model):
     title=models.CharField(max_length=250)
     price=models.IntegerField(default=0)
     rating=models.IntegerField(default=0)
-    created_at=models.DateTimeField(default=0))
     
 
     def __str__(self):
